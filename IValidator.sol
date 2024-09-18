d9758f866f34fa48fd3e65c1ef150afdc5ce3989
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-09-17 18:57:30 -0700

    Update IValidator.sol

--- IValidator.sol
+++ IValidator.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+/**
+ * @title IValidator
+ * @dev Interface for Validator contracts used with DeedNFT.
+ */
+interface IValidator {
+    /**
+     * @dev Returns the token URI for a given token ID.
+     * @param tokenId ID of the token.
+     * @return URI string.
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+
+    /**
+     * @dev Returns the default operating agreement URI.
+     * @return URI string.
+     */
+    function defaultOperatingAgreement() external view returns (string memory);
+
+    /**
+     * @dev Returns the name associated with an operating agreement URI.
+     * @param uri_ URI of the operating agreement.
+     * @return Name string.
+     */
+    function operatingAgreementName(string memory uri_) external view returns (string memory);
+}
 
